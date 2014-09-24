d96d62dfad10d688445f1fdae6a136f4a09527e2
Nicolas Mollet <nico.mollet@gmail.com>
2014-09-24 13:32:18 +0200

    Use ashleydw lightbox for modal gallery: http://ashleydw.github.io/lightbox/

--- footer.php
+++ footer.php
@@ -38,42 +38,6 @@
     </div>
 </div>
 
-<!-- The Bootstrap Image Gallery lightbox, should be a child element of the document body -->
-<div id="blueimp-gallery" class="blueimp-gallery">
-	<!-- The container for the modal slides -->
-	<div class="slides"></div>
-	<!-- Controls for the borderless lightbox -->
-	<h3 class="title"></h3>
-	<a class="prev">‹</a>
-	<a class="next">›</a>
-	<a class="close">×</a>
-	<a class="play-pause"></a>
-	<ol class="indicator"></ol>
-	<!-- The modal dialog, which will be used to wrap the lightbox content -->
-	<div class="modal fade">
-		<div class="modal-dialog">
-			<div class="modal-content">
-				<div class="modal-header">
-					<button type="button" class="close" aria-hidden="true">&times;</button>
-					<h4 class="modal-title"></h4>
-				</div>
-				<div class="modal-body next"></div>
-				<div class="modal-footer">
-					<button type="button" class="btn btn-default pull-left prev">
-						<i class="glyphicon glyphicon-chevron-left"></i>
-						Previous
-					</button>
-					<button type="button" class="btn btn-primary next">
-						Next
-						<i class="glyphicon glyphicon-chevron-right"></i>
-					</button>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
-<!-- /#modal-gallery -->
-
 <div class="modal fade do-not-print" id="modal-default" tabindex="-1" aria-hidden="true" role="dialog">
   <div class="modal-dialog">
     <div class="modal-content">
