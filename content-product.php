42253a43b3ca1b6c0dcd367a4cf05bdce095e375
Nicolas Mollet <nico.mollet@gmail.com>
2014-09-30 18:07:57 +0200

    Woocommerce compatibility

---
+++ content-product.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * The template for displaying product content within loops.
+ *
+ * Override this template by copying it to yourtheme/woocommerce/content-product.php
+ *
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates
+ * @version     1.6.4
+ */
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+?>
+<div class="<?php echo $classes?>">
+
+	
+	<?php do_action( 'woocommerce_before_shop_loop_item' ); ?>
+
+	<div class="product"><a href="<?php the_permalink(); ?>">
+
+		<?php
+			/**
+			 * woocommerce_before_shop_loop_item_title hook
+			 *
+			 * @hooked woocommerce_show_product_loop_sale_flash - 10
+			 * @hooked woocommerce_template_loop_product_thumbnail - 10
+			 */
+			 
+			do_action( 'bs_before_shop_loop_item_title' ); 
+		?>
+
+		<h3><?php the_title(); ?></h3>
+
+		<?php
+			/**
+			 * woocommerce_after_shop_loop_item_title hook
+			 *
+			 * @hooked woocommerce_template_loop_price - 10
+			 */
+			do_action( 'woocommerce_after_shop_loop_item_title' );
+		?>
+
+	</a>
+
+	<?php do_action( 'woocommerce_after_shop_loop_item' ); ?>
+	</div>
+</div>
