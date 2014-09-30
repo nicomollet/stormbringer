53eaf910d5571b18214262eb03b22aa1ac0fdb51
Nicolas Mollet <nico.mollet@gmail.com>
2014-09-30 23:39:33 +0200

    strombringer admin color scheme

--- inc/admin/cleanup.php
+++ inc/admin/cleanup.php
@@ -1,16 +1,29 @@
 <?php
 
-
-
-/**
- * Remove unnecessary dashboard widgets
- *
- * @link http://www.deluxeblogtips.com/2011/01/remove-dashboard-widgets-in-wordpress.html
- */
+// Remove unnecessary dashboard widgets
 function stormbringer_remove_dashboard_widgets() {
   remove_meta_box('dashboard_incoming_links', 'dashboard', 'normal');
   remove_meta_box('dashboard_plugins', 'dashboard', 'normal');
   remove_meta_box('dashboard_primary', 'dashboard', 'normal');
   remove_meta_box('dashboard_secondary', 'dashboard', 'normal');
+	//Remove  WordPress Welcome Panel
+	remove_action('welcome_panel', 'wp_welcome_panel');
 }
 add_action('admin_init', 'stormbringer_remove_dashboard_widgets');
+
+// Admin color scheme
+function stormbringer_admin_color_schemes() {
+	$theme_dir = get_template_directory_uri();
+	wp_admin_css_color( 'stormbringer', __( 'StormBringer' ),
+		$theme_dir . '/inc/admin/colors.css',
+		array( '#8e0c70', '#5f084b', '#e0feff', '#006F71' )
+	);
+}
+add_action('admin_init', 'stormbringer_admin_color_schemes');
+
+// Remove WP menu in admin toolbar
+function stormbringer_remove_wp_logo_from_admin_bar() {
+	global $wp_admin_bar;
+	$wp_admin_bar->remove_menu('wp-logo');
+}
+add_action( 'wp_before_admin_bar_render', 'stormbringer_remove_wp_logo_from_admin_bar', 7 );
