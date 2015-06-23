88b056189353e011076e73171a86a9cc1d081f05
Nico Mollet <nico.mollet@gmail.com>
2015-06-23 14:41:34 +0200

    Gravity Forms: ajax forms javascript non-render blocking

--- inc/plugins/gravityforms.php
+++ inc/plugins/gravityforms.php
@@ -322,3 +322,13 @@ add_filter("gform_init_scripts_footer", "gravityforms_footer_noblockrender");
 function gravityforms_footer_noblockrender() {
 	return true;
 }
+add_filter( 'gform_cdata_open', 'wrap_gform_cdata_open' );
+function wrap_gform_cdata_open( $content = '' ) {
+    $content = 'document.addEventListener( "DOMContentLoaded", function() { ';
+    return $content;
+}
+add_filter( 'gform_cdata_close', 'wrap_gform_cdata_close' );
+function wrap_gform_cdata_close( $content = '' ) {
+    $content = ' }, false );';
+    return $content;
+}
