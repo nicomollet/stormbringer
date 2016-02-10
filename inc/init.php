1b3e5fbbb11fbcfe7e985179049a0d7a05921f98
Nico Mollet <nico.mollet@gmail.com>
2016-02-10 17:05:46 +0100

    Cuztom

--- inc/init.php
+++ inc/init.php
@@ -6,6 +6,8 @@ function stormbringer_support() {
 	add_theme_support( 'post-thumbnails' );
     add_theme_support( 'title-tag' );
 
+	locate_template( 'inc/library/cuztom/cuztom.php', true );         // Cuztom library
+
 	if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ){
 		add_theme_support( 'woocommerce' );
 	}
