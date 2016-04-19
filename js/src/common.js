cc76568501e20ad1c4029a225f136852f9389ca7
Nico Mollet <nico.mollet@gmail.com>
2016-04-19 16:56:04 +0200

    Tracking event for Gravity Forms submission

--- js/src/common.js
+++ js/src/common.js
@@ -104,7 +104,7 @@ $(document).ready(function () {
     var at = / at /;
     var dot = / dot /g;
     var addr = $(spt).attr("title").replace(at,"@").replace(dot,".");
-    $(spt).after('<a class="cryptemail" href="mailto:'+addr+'" alt="'+$(spt).attr("title")+'">'+addr+'</a>')
+    $(spt).after('<a class="cryptemail" href="mailto:'+addr+'" title="'+$(spt).attr("title")+'">'+addr+'</a>')
       .hover(function(){window.status="Contact";}, function(){window.status="";});
     $(spt).remove();
   });
