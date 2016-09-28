eabbcbc07e7d14733c74bee01f98662611d167af
Nico Mollet <nico.mollet@gmail.com>
2016-09-28 11:13:30 +0200

    Storefront child theme: perfectday

--- Gruntfile.js
+++ Gruntfile.js
@@ -33,7 +33,7 @@ module.exports = function(grunt) {
           'mediaqueries',
           'csstransforms',
           'cssvhunit',
-          'cssvwunit'
+          'cssvwunit',
           'objectfit'
         ]
       }
