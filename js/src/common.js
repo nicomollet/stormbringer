2daa28680d84c57d8421138e33e2e78f0fc66add
Nico Mollet <nico.mollet@gmail.com>
2017-05-23 10:52:09 +0200

    Fix scroll actions

--- js/src/common.js
+++ js/src/common.js
@@ -11,7 +11,7 @@ jQuery(function($) { // DOM is now ready and jQuery's $ alias sandboxed
       if (st > headerHeight && st > lastScrollTop){
         $('#navigation').addClass('navbar-outofview').addClass('navbar-stuck');
       }
-      if(st < (headerHeight-navHeight)){
+      if(st <= (headerHeight)){
         $('#navigation').removeClass('navbar-stuck');
       }
       if (st > lastScrollTop){ // going down
