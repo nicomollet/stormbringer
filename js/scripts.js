93f88233025efd50da823df92b6da06c0ef5cbf0
Nico Mollet <nico.mollet@gmail.com>
2017-09-25 11:05:45 +0200

    Remove Gravity Forms submission to Analytics event

--- js/scripts.js
+++ js/scripts.js
@@ -2008,13 +2008,6 @@ jQuery(function($) { // DOM is now ready and jQuery's $ alias sandboxed
       $(this).attr('readonly','readonly');
   });
 
-  // Tracking event for Gravity Forms submission
-  $(document).bind('gform_confirmation_loaded', function(event, formId){
-    if (typeof ga === 'function') {
-      ga('send', 'event', 'Forms', 'Submission', 'formid_'+formId);
-    }
-  });
-
   // Email obfuscation
   $("span.cryptemail").each(function(){
     var spt = $(this);
